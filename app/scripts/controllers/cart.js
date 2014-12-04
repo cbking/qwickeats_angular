da896444d153c0b7af423324f5e6892331ca40a9
Christian Cocking <christianc750@gmail.com>
2014-12-04 13:31:50 -0500

    Modifed the input buttons to correctly bind the relevant data to the scope

---
+++ app/scripts/controllers/cart.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name qwickeatsApp.controller:CartCtrl
+ * @description
+ * # CartCtrl
+ * Controller of the qwickeatsApp
+ */
+angular.module('qwickeatsApp')
+  .controller('CartCtrl', function ($scope) {
+  	
+  	$scope.cart = 'Need the Cart Service to Figure out the Cart';
+
+
+
+  });
--- app/scripts/controllers/menu.js
+++ app/scripts/controllers/menu.js
@@ -14,14 +14,34 @@ angular.module('qwickeatsApp')
     $scope.selectedMenuItem = '';
     $scope.selectedmenuItemOptions = '';
     $scope.selecteditemPrice = 0;
+    $scope.required = {};
+    $scope.optional = {};
+    $scope.formData = {};
+
+    $scope.cleanUpOptional = function ()
+    {
+        console.log($scope.optional);
+        for (var item in $scope.optional)
+        {
+            if ($scope.optional[item] === false)
+            {
+              delete $scope.optional[item];
+            } 
+        }
+        $scope.$apply;
+        console.log($scope.optional);
+    };
 
     $scope.getMenuItemOptions = function (categoryIndex, menuItemIndex ,menuItemName)
     {
-			// Uses data from the view to get the correct data for the menu item and sets it in the scope
-			// Do I need the digest loop here?
+    	// Uses data from the view to get the correct data for the menu item and sets it in the scope
+    	// Do I need the digest loop here?
     	$scope.selectedMenuItem = menuItemName;
     	$scope.selectedmenuItemOptions = menu.data.categories[categoryIndex].menu_items[menuItemIndex].menu_item_options;
     	$scope.selecteditemPrice = menu.data.categories[categoryIndex].menu_items[menuItemIndex].price;
+        $scope.required = {};
+        $scope.optional = {};
+        $scope.formData = {};
     };
 
   }]);
